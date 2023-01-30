f172c64f7c8a9d29601856c640c142e977a0193a
alexsavinov <spell477@gmail.com>
2023-01-30 15:10:53 +0200

    homepage

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -18,9 +18,13 @@ import {CarouselConfig} from "ngx-bootstrap/carousel";
 })
 export class HomepageComponent implements OnInit {
   slides = [
-    {image: 'assets/logo.png', text: 'First'},
-    // {image: 'assets/swagger.png', text: 'Second'},
-    // {image: 'assets/django.png', text: 'Third'}
+    {image: 'assets/news_title.jpg', text: 'I am a creative, patient, and licensed high school English teacher. ' +
+        'I have excellent communication skills\n' +
+        'and novel approaches to creating lesson plans. I have a 90% satisfaction rate from both students and\n' +
+        'peers. I look forward to applying my skills and to contributing to high-quality secondary education at\n' +
+        'your school.'},
+    // {image: 'assets/news_title.jpg', text: 'Second'},
+    // {image: 'assets/news_title.jpg', text: 'Third'}
   ];
   noWrapSlides = false;
   showIndicator = true;
